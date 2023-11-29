fb4201c4b4b637cfe42f6075a37a4a8713701d03
O.O_ixn <144454361+OOixn@users.noreply.github.com>
2023-11-29 10:59:00 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -2,7 +2,7 @@ function solution(a, b, c) {
     let result = 0;
     
     if (a === b && b === c) {
-        result = (a + b + c) * (a * a + b * b + c * c) * (a * a * a + b * b * b + c * c * c);
+        result = (a + b + c) * (a * a + b * b + c * c) * (a ** 3 + b ** 3 + c **3);
     } else if (a === b || b === c || a === c) {
         result = (a + b + c) * (a * a + b * b + c * c);
     } else {
