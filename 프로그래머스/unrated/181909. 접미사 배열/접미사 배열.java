062d9392727796e7db525c81d2711ddb8546d55e
O.O_ixn <144454361+OOixn@users.noreply.github.com>
2023-12-22 09:41:25 +0900

    [level unrated] Title: 접미사 배열, Time: 0.25 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -1,13 +1,17 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = new String[my_string.length()];
+        StringBuilder a = new StringBuilder(my_string);
+        ArrayList<String> b = new ArrayList<>();
         
-        for(int i = 0; i < my_string.length(); i++){
-            answer[i] = my_string.substring(i);
+        for (int i = 0; i < my_string.length(); i++) {
+            b.add(a.substring(i));
         }
-        
+
+        String[] answer = new String[b.size()];
+        answer = b.toArray(answer);
         Arrays.sort(answer);
         
         return answer;
