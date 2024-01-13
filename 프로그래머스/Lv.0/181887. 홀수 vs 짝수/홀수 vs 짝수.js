983cae5fca8fb35910534ec4b12e36609b11daed
O.O_ixn <144454361+OOixn@users.noreply.github.com>
2024-01-13 09:59:07 +0900

    [level Lv.0] Title: 홀수 vs 짝수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,18 @@
+function solution(num_list) {
+    let a = 0;
+    let b = 0;
+    
+    for(let i = 0; i < num_list.length; i++){
+        if(i % 2 === 0){
+            a += num_list[i];
+            
+            } else {
+            b +=num_list[i];
+            
+        }
+    }
+     if( a > b ){
+         return a;
+     } else return b;
+}
+
