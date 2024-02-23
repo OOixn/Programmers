a83b5d222bcadd07e47dd520301a34ed3d84a76e
O.O_ixn <144454361+OOixn@users.noreply.github.com>
2024-02-23 09:18:28 +0900

    [level 0] Title: 배열의 유사도, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -0,0 +1,12 @@
+function solution(s1, s2) {
+    var answer = 0;
+    s1.map((e) => {
+        for(let i = 0; i < s2.length; i++){
+            if(e === s2[i]){
+                answer++;
+            }
+        }
+    }
+          )
+    return answer;
+}
