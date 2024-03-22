2a70ead0b54a763a60f3c9b693507ae18d5e9c6d
O.O_ixn <144454361+OOixn@users.noreply.github.com>
2024-03-22 09:13:31 +0900

    [level 0] Title: 안전지대, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.js
@@ -0,0 +1,35 @@
+function solution(board) {
+  const N = board.length
+  const dx = [-1, 1, 0, 0, -1, -1, 1, 1]
+  const dy = [0, 0, -1, 1, -1, 1, -1, 1]
+
+  const booms = []
+  for (let x = 0; x < N; x++) {
+    for (let y = 0; y < N; y++) {
+      if (board[x][y] === 1) {
+        booms.push([x, y])
+      }
+    }
+  }
+
+  booms.forEach(([x, y]) => {
+    for (let i = 0; i < 8; i++) {
+      const nx = x + dx[i]
+      const ny = y + dy[i]
+      if (0 <= nx && nx < N && 0 <= ny && ny < N) {
+        board[nx][ny] = 1
+      }
+    }
+  })
+
+  let count = 0
+  for (let x = 0; x < N; x++) {
+    for (let y = 0; y < N; y++) {
+      if (board[x][y] === 0) {
+        count++
+      }
+    }
+  }
+
+  return count
+}
