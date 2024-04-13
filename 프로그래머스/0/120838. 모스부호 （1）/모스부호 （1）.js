ba67a3a90887bb6a8d3ee57959471185e5889daf
O.O_ixn <144454361+OOixn@users.noreply.github.com>
2024-04-13 17:00:28 +0900

    [level 0] Title: 모스부호 (1), Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,17 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    var answer = '';
+    const letterArr = letter.split(" ");
+    
+    letterArr.forEach((item) => {
+        answer += morse[item];
+    })
+    
+    return answer;
+}
