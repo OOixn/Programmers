8a0214e0fb53930ab8b4d9c1cd8976a235387c38
O.O_ixn <144454361+OOixn@users.noreply.github.com>
2024-06-22 02:29:22 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,12 @@
+function solution(t, p) {
+    var answer = 0;
+    
+    for(let i = 0; i <= t.length - p.length; i++){
+        let substring = t.substring(i, i + p.length);
+        if(substring <= p) {
+            answer ++;
+        }
+    }
+    
+    return answer;
+}
