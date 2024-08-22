faf02c245ee67fc90448d1d2b21a2569275f90c6
O.O_ixn <144454361+OOixn@users.noreply.github.com>
2024-08-22 14:15:39 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    var answer = s.split(" ");
+    for(let i = 0; i < answer.length; i++){
+        if (answer[i].length > 0) {
+            answer[i] = answer[i][0].toUpperCase() + answer[i].slice(1).toLowerCase();
+        }
+    }
+    return answer.join(" ");
+}
