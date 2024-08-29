007e7e3696936feeeb22b6306e926a86c4e3fece
O.O_ixn <144454361+OOixn@users.noreply.github.com>
2024-08-29 20:03:23 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,12 @@
+function solution(N) {
+    let ans = 0;
+    while (N > 0) {
+        if (N % 2 === 0) {
+            N /= 2;
+        } else {
+            N -= 1;
+            ans++;
+        }
+    }
+    return ans;
+}
